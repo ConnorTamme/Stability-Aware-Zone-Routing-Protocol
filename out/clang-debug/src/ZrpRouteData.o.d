540caafe75391d9c232e6a13e2a350c4128b8aae
Connor Tamme <tammeconnor@gmail.com>
2026-02-10 17:26:46 -0600

    Added in BRP code as well

---
+++ out/clang-debug/src/ZrpRouteData.o.d
@@ -0,0 +1,36 @@
+../out/clang-debug/src/ZrpRouteData.o: ZrpRouteData.cc ZrpRouteData.h \
+  ../../inet-4.5.4/src/inet/networklayer/common/L3Address.h \
+  ../../inet-4.5.4/src/inet/linklayer/common/MacAddress.h \
+  ../../inet-4.5.4/src/inet/common/INETDefs.h \
+  ../../inet-4.5.4/src/inet/common/precompiled_debug.h \
+  ../../inet-4.5.4/src/inet/common/precompiled.h \
+  /home/mint/default_workspace/omnetpp-6.3.0/include/omnetpp.h \
+  ../../inet-4.5.4/src/inet/opp_defines.h \
+  ../../inet-4.5.4/src/inet/features.h \
+  ../../inet-4.5.4/src/inet/common/Compat.h \
+  ../../inet-4.5.4/src/inet/common/InitStages.h \
+  ../../inet-4.5.4/src/inet/common/InitStageRegistry.h \
+  ../../inet-4.5.4/src/inet/networklayer/common/ModuleIdAddress.h \
+  ../../inet-4.5.4/src/inet/networklayer/common/ModulePathAddress.h \
+  ../../inet-4.5.4/src/inet/networklayer/contract/clns/ClnsAddress.h \
+  ../../inet-4.5.4/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+  ../../inet-4.5.4/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+  ../../inet-4.5.4/src/inet/networklayer/common/InterfaceToken.h
+ZrpRouteData.h:
+../../inet-4.5.4/src/inet/networklayer/common/L3Address.h:
+../../inet-4.5.4/src/inet/linklayer/common/MacAddress.h:
+../../inet-4.5.4/src/inet/common/INETDefs.h:
+../../inet-4.5.4/src/inet/common/precompiled_debug.h:
+../../inet-4.5.4/src/inet/common/precompiled.h:
+/home/mint/default_workspace/omnetpp-6.3.0/include/omnetpp.h:
+../../inet-4.5.4/src/inet/opp_defines.h:
+../../inet-4.5.4/src/inet/features.h:
+../../inet-4.5.4/src/inet/common/Compat.h:
+../../inet-4.5.4/src/inet/common/InitStages.h:
+../../inet-4.5.4/src/inet/common/InitStageRegistry.h:
+../../inet-4.5.4/src/inet/networklayer/common/ModuleIdAddress.h:
+../../inet-4.5.4/src/inet/networklayer/common/ModulePathAddress.h:
+../../inet-4.5.4/src/inet/networklayer/contract/clns/ClnsAddress.h:
+../../inet-4.5.4/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+../../inet-4.5.4/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+../../inet-4.5.4/src/inet/networklayer/common/InterfaceToken.h:
